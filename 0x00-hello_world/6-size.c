7d47249834c2574e13cb9af7a630503483364b09
Yara20031964 <yara200359@gmail.com>
2023-06-15 16:56:06 -0700

    yara

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: "A c program that display by using puts"
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)", sizeof(char));
+	printf("Size of a int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)", sizeof(long long int));
+	printf("Size of a float: %f byte(s)", sizeof(float));
+	return (0);
+}
