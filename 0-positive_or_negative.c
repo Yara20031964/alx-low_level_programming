34064bd0868d8dba8749d8b753d1106ac80a3cdf
Yara20031964 <yara200359@gmail.com>
2023-06-19 14:03:23 -0700

    yara

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: "A c program that display by using printf"
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(n>0)
+		printf("is positive %d\n",n);
+        else if(n=0)
+                printf("is zero %d\n",n);
+	else
+		printf("is negative %d\n",n);
+
+	return (0);
+}
