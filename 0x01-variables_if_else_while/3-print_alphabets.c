2a32ffb8076f1c4cb34149c246a6741cf7a4c849
Yara20031964 <yara200359@gmail.com>
2023-06-19 16:52:45 -0700

    yara

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: "A c program that display by using printf"
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char lowercase = 'a';
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase += 1;
+}
+putchar('\n');
+char uppercase = 'A';
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase += 1;
+}
+putchar('\n');
+return (0);
+}
