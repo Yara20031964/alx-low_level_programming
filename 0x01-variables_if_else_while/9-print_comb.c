784980f2e3d2a7e7d26eeb690d12f4c46b55aa5a
Yara20031964 <yara200359@gmail.com>
2023-06-19 18:27:05 -0700

    yara

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,19 +15,13 @@ int main(void)
 int n = 0;
 while (n < 10)
 {
-if (n == 9)
-{
 putchar(n + '0');
-putchar('$');
-++n;
-}
-else
+if (n != 9)
 {
-putchar(n + '0');
 putchar(',');
 putchar(' ');
-++n;
 }
+++n;
 }
 putchar('\n');
 return (0);
