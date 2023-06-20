32402a69bc094564482becf8a38d9d13b7a3d97c
Yara20031964 <yara200359@gmail.com>
2023-06-19 19:41:13 -0700

    yara

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: "A c program that display by using printf"
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n = 0;
+int m = 0;
+int k = 0;
+for (n = 0; n <= 98; ++n)
+{
+for (m = 0; m <= 99; ++m)
+{
+putchar(n / 10 + '0');
+putchar(n % 10 + '0');
+putchar(' ');
+putchar(m / 10 + '0');
+putchar(m % 10 + '0');
+if (n == 98 && m == 99)
+continue
+putchar(',');
+putchar(' ');
+return (0);
+}
