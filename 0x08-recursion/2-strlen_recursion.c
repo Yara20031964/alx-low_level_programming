74d1dbc725c9897be9c66bf944fe62fc9084772d
Yara20031964 <yara200359@gmail.com>
2023-07-22 20:13:45 -0700

    yarar

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen_recursion-print a string, followed by a new line
+*@s:input
+*Return: Always 0 (Success)
+*/
+
+int _strlen_recursion(char *s)
+{
+if (*s > '\0')
+{
+int ntr = 0;
+ntr += _strlen_recursion(s + 1) + 1;
+}
+return (ntr);
+}
