97ec1a467ee608a4fc4d502281873530c4959403
Yara20031964 <yara200359@gmail.com>
2023-07-24 01:24:33 -0700

    yara

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: "A c program that display by using printf"
+ *
+ *@argc : input
+ *
+ *@argv : input
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
