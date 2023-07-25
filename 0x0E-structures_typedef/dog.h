dce942eaa44f2efe2422fc380fc22cb4eeeea612
Yara20031964 <yara200359@gmail.com>
2023-07-25 02:17:48 -0700

    yara

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog-print a string, followed by a new line
+*@age:input
+*@owner:input
+*@name:input
+*Return: Always 0 (Success)
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
