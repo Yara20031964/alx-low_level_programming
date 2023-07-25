d11bf768f93f9a142459648e801cd7b292dbc1b4
Yara20031964 <yara200359@gmail.com>
2023-07-25 02:22:39 -0700

    yara

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog-print a string, followed by a new line
+*@age:input
+*@owner:input
+*@name:input
+*Return: Always 0 (Success)
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
