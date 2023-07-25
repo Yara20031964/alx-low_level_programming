ef184141fac839069583383d6f03b5d6d1c788ff
Yara20031964 <yara200359@gmail.com>
2023-07-25 02:49:51 -0700

    yara

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 if (d)
 {
 printf("Name: %s\n", d->name ? d-> name : "(nil)");
-printf("Age: %d\n", d->age ? d-> age : "(nil)");
+printf("Age: %f\n", d->age ? d-> age : "(nil)");
 printf("Owner: %s\n", d->owner ? d-> owner : "(nil)");
 }
 }
