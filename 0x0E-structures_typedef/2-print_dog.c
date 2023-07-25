60e7a2a01260c734f2823ba9438fc0ff8718e28b
Yara20031964 <yara200359@gmail.com>
2023-07-25 02:47:40 -0700

    yara

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+*init_dog-print a string, followed by a new line
+*@age:input
+*@owner:input
+*@name:input
+*@d:input
+*Return: Always 0 (Success)
+*/
+
+void print_dog(struct dog *d)
+{
+if (d)
+{
+printf("Name: %s\n", d->name ? d-> name : "(nil)");
+printf("Age: %d\n", d->age ? d-> age : "(nil)");
+printf("Owner: %s\n", d->owner ? d-> owner : "(nil)");
+}
+}
