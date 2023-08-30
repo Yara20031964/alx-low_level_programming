e35f3f8f12e9db229c5377ffccf96debe0dd6e71
root <yara200359@gmail.com>
2023-08-30 16:53:16 -0700

    yara

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+
+/**
+*_pow_recursion - print a string, followed by a new line
+*
+*@x:input pointer of string
+*
+*@y:input pointer of string
+*
+*Return: Always 0 (Success)
+*/
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (0);
+}
+else if (y < 0)
+{
+return (-1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
