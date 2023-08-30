50cf21f516610035b498fdc157499309eed6d047
root <yara200359@gmail.com>
2023-08-30 14:24:26 -0700

    yara

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_puts_recursion - print a string, followed by a new line
+*
+*@s:input pointer of string
+*
+*Return: Always 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar("\n");
+}
+else
+{
+_putchar(*s);
+_void _puts_recursion(s + 1);
+}
+
