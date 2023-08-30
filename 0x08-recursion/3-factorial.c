325499b8d23e1fa29fa927861d9283f471d969d0
root <yara200359@gmail.com>
2023-08-30 15:36:22 -0700

    yara

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+/**
+*factorial - print a string, followed by a new line
+*
+*@n:input pointer of string
+*
+*Return: Always 0 (Success)
+*/
+
+int factorial(int n)
+{
+if (n==0)
+{
+return (0);
+}
+else
+{
+return (n * factorial (n - 1);
+}
+}
