8af4f1f64de83cdd2cabae2a873b8f6b3e683a36
root <yara200359@gmail.com>
2023-08-30 18:36:02 -0700

    yara

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdioh>
+
+/**
+*is_prime_number-print a string, followed by a new line
+*@n:input
+*@othrn:input
+*Return: Always 0 (Success)
+*/
+int ch_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+return (ch_prime(n, 2));
+}
+
+/**
+*ch_prime a string, followed by a new line
+*@n:input
+*othrn:input
+*Return: Always 0 (Success)
+*/
+
+int ch_prime(int n, int othrn);
+{
+if (othrn >= n && n > 1)
+
+return (1);
+
+else if (n % othrn == 0 || n <=1)
+
+return (0);
+
+else
+
+return (ch_prime(ch_prime(n, othrn + 1));
+}
