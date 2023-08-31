13b0567aa957fe94dd79b5b544be4e32671d7e89
root <yara200359@gmail.com>
2023-08-30 18:18:09 -0700

    yara

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+*_sqrt_recursion-print a string, followed by a new line
+*@n:input
+*@i:input
+*Return: Always 0 (Success)
+*/
+int square(int n, int i);
+int _sqrt_recursion(int n)
+{
+return (square(n, 1));
+}
+
+/**
+*square-print a string, followed by a new line
+*@n:input
+*@i:input
+*Return: Always 0 (Success)
+*/
+
+int square(int n, int i)
+{
+if (i * i == n)
+
+return (i);
+
+else if (i * i < n)
+
+return (square(n, i + 1));
+
+else
+
+return (-1);
+}
