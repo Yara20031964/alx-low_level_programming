15cddf6c1e1a9ad4cbe576c264922e5b15004ec5
root <yara200359@gmail.com>
2023-09-01 16:50:18 -0700

    yara

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -print program name
+*@argc : argumaent count
+*@argv : argument vector
+*return zero
+*/
+
+main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s/n", argv[0]);
+return (0);
+}
