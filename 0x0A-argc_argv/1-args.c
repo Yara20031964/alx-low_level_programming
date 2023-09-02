2fdfa31ac50f18037a84776c2cd40c8786100aff
root <yara200359@gmail.com>
2023-09-01 17:16:34 -0700

    yara

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *@argc: 'the program's description'
+ *@argv: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
