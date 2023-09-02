d1301d834c82fa7b9d86174813cae3bafdf3d09c
root <yara200359@gmail.com>
2023-09-01 18:44:46 -0700

    yara

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,3 +1,5 @@
+
+
  #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -13,24 +15,19 @@ int main(int argc, char const *argv[])
 {
 int mul = 0;
 int i;
-int count = 0;
 for (i = 1; i < argc; i++)
 {
 if (*argv[i] < '0' || *argv[i] > '9')
 {
 printf("Error\n");
 return (1);
-break;
 }
-count = 1;
 }
-if (count == 0)
-{
+
 for (i = 1; i < argc; i++)
 {
-mul += atoi(*argv[i]);
+mul += atoi(argv[i]);
 }
 printf("%d\n", mul);
 return (0);
-}
 }
