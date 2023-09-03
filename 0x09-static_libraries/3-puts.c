28cf7c4ce93e7c746f67a4e0b596a33f4fe7cacc
root <yara200359@gmail.com>
2023-09-03 09:13:04 -0700

    yara

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*_puts - Entry point
+*@str: parameter
+* main - entry point
+* Return: Always 0 (Success)
+*/
+void _puts(char *str)
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
