17d0210399a8c6285b6c0f0ec658acbdf7fb1325
root <root@YARA>
2023-09-06 03:24:28 +0300

    yara

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*create_array-print a string, followed by a new line
+*@size:input
+*@c:input
+*Return: Always 0 (Success)
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size == Null)
+		return (0);
+
+	while (size--)
+		ptr[size] = c;
+	return (ptr);
+}
