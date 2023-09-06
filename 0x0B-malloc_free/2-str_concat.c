897dd47cc66b762223a3d34dc0707ac043a67327
root <root@YARA>
2023-09-07 00:12:20 +0300

    yara

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 		return (0);
 	if (s2 == 0)
 		return (0);
-	for (size1 = 0; str[size1] != '\0'; size1++)
+	for (size1 = 0; s1[size1] != '\0'; size1++)
 		;
-	for (size2 = 0; str[size2] != '\0'; size2++)
+	for (size2 = 0; s2[size2] != '\0'; size2++)
 	;
 	ptr = malloc((size1 + size2) + 2);
 
