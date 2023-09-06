b5da43ce82fbeac1d076819cc136ecf9fb9fea94
root <root@YARA>
2023-09-06 22:13:46 +0300

    yara

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 
 /**
-* *_strdup - print a string, followed by a new line
-*@srt:input
-*Return: Always 0 (Success)
+* *_strdup-print a string, followed by a new line
+* @srt: input it is a parameter
+* Return: Always 0 (Success)
 */
 
 char *_strdup(char *str)
 {
 	int size, i;
+	char *ptr;
 
 	if (str == 0)
 		return (0);
 	for (size = 0; str[size] != '\0'; size++)
 		;
-	char *ptr;
-
 	ptr = malloc(size + 1);
 
 	if (ptr == 0)
 		return (0);
 		for (i = 0; i < size; i++)
 			str[i] = ptr[i];
+
 	return (ptr);
+}
