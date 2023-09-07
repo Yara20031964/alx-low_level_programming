18a4a26271ff86c4dbf49d845a8333bfe2a012b5
root <root@YARA>
2023-09-07 21:14:39 +0300

    yara

---
+++ 0-malloc_checked.c/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - Entry point
+ *
+ *@b: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(sizeof(unsigned int) * b);
+
+	if (ptr == 0)
+		exit(98);
+	return (ptr);
+
