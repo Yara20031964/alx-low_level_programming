50a134192c6040a8a4c1cfd4c72ac582c2d06a01
root <root@YARA>
2023-09-08 04:18:23 +0300

    yara

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _calloc - duplicate to new memory space location
+ * @nmemb: char
+ * @size: char
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	int *ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == 0 || nmemb == 0 || size == 0)
+		return (0);
+	for (i = 0; i < nmemb * size; ++i)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
