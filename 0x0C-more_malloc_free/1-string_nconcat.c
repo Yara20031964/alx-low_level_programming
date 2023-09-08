ffe71beece4c4c9f70f17a8bc22617c9838cf581
root <root@YARA>
2023-09-08 04:06:03 +0300

    yara

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size1, size2, i, j;
 	char *ptr;
 
-	for (size1 = 0; s1[size1] != '\0'; size1++)
+	for (size1 = 0; s1 && s1[size1] != '\0'; size1++)
 	;
-	for (size2 = 0; s2[size2] != '\0'; size2++)
+	for (size2 = 0; s2 && s2[size2] != '\0'; size2++)
 	;
 	if (n < size2)
 		ptr = malloc((size1 + n + 1));
