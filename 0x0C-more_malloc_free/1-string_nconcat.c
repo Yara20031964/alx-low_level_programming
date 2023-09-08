45ad98c7e7bd80d3a8d16ccaa52b3da7b5aff501
root <root@YARA>
2023-09-08 03:17:22 +0300

    yara

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - duplicate to new memory space location
+ * @s1: char
+ * @s2: char
+ * @n : input
+ * Return: 0
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int size1, size2, i, j;
+	char *ptr;
+
+	if (s1 == 0)
+		s1 = "\0";
+	if (s2 == 0)
+		s2 = "\0";
+	for (size1 = 0; s1[size1] != '\0'; size1++)
+	;
+	for (size2 = 0; s2[size2] != '\0'; size2++)
+	;
+	if (n < size2)
+		s = malloc(sizeof(char) * (size1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (size1 + size2 + 1));
+
+	if (ptr == 0)
+		return (0);
+	for (i = 0; i < size1; i++)
+	{
+		ptr[i] = s1[i]
+	}
+	if (n < size2)
+	{
+		for (j = 0; j < n; j++)
+			ptr[i] = s2[i];
+		++i;
+	}
+	else
+	{
+		for (j = 0; j < size2; j++)
+			ptr[i] = s2[i];
+		i++;
+	}
+
+	return (ptr);
+}
