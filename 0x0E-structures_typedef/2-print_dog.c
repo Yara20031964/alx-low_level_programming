718cfebe9c20801fe33593d90608c5949fe2ae70
root <root@YARA>
2023-09-12 15:50:40 +0300

    yara

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function
+ * @d: is paramter
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)")
+		printf("Age: %f\n", d-age> ? d->age : "(nil)")
+		printf("owner: %s\n", d->owner ? d->owner : "(nil)")
+	}
+}
+
