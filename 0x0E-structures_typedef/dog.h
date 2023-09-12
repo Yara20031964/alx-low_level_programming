2e58f48887eebf278103411fbcaa628eb4e62c60
root <root@YARA>
2023-09-12 04:57:02 +0300

    yara

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name : name
+ * @age : age
+ * @owner : owner's name
+ *
+ * Description: just struct named dog
+ */
+struct dog
+{
+	char *name:
+	float age;
+	char *owner;
+};
+#endif
