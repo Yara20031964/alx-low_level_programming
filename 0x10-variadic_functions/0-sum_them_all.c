f6b99bd9ff277da0918f75086ab2810730b412a7
root <root@YARA>
2023-09-15 03:11:54 +0300

    yara

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_function.h"
+
+/**
+ * sum_them_all - sums variable arguments
+ * @n:argument
+ * @...: argument
+ * Return: 0 or sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum, i;
+
+	if (!n)
+		return (0);
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
