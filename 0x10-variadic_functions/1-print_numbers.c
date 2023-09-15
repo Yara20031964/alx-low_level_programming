11420253744b92f6921f40890704f16e79b36b63
root <root@YARA>
2023-09-15 03:59:16 +0300

    yara

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio>
+
+/**
+ * print_numbers - function name
+ * @separator: argument
+ * @n: argument
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
