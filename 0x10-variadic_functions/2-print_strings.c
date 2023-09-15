95da70187994ca39a4c456dbea6b469a18fb85f8
root <root@YARA>
2023-09-15 04:24:48 +0300

    yara

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,10 +16,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char *) == NULL)
+		str = va_arg(args, char *);
+
+		if (str == NULL)
 			printf("(nil)");
 		else
-		printf("%s", va_arg(args, char *));
+		printf("%s", str);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
