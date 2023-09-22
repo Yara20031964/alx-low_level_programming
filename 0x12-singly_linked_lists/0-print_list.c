8a925aab7895df5f6d257a3a525bb723e3f13990
root <root@YARA>
2023-09-22 05:03:34 +0300

    this is the first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * str_len - function return len
+ * @s:string
+ * Return: return len or 0
+ */
+int str_len(char*s)
+{
+	int i = 0;
+	
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - function
+ * @h:argument of function
+ * Return: return number of nodes an len
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	while (h)
+	{
+		printf("[%d] %s\n", str_len(h->str),h->str ? h-str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+now we learn linked list
