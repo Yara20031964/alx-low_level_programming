d2c23f855dcc3330cda797bef0de2edd129c324a
root <root@YARA>
2023-09-27 02:58:20 +0300

    this is the  4 task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint- funtion delete allthing
+ * @head: parameter
+ * Return: no return it is void fun.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node = head;
+
+	while (node)
+	{
+		node = node->next;
+		free(node);
+	}
+}
+
+
