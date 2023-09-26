5448b3bf2f9da77d5110f1042d1ee88b9df20c89
root <root@YARA>
2023-09-27 01:55:46 +0300

    this is the  1 task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len-function return number of element
+ * @h: arguments
+ * Return: return no.of element
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
