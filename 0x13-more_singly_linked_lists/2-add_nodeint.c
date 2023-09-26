e90e9e4d156c9ec6472d635871c3581095886d90
root <root@YARA>
2023-09-27 02:14:04 +0300

    this is the  2 task

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * creatnode- function create node
+ * @data: parameter
+ * Return: return newnode
+ */
+listint_t *creatnode(const int data)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+
+	newnode->n = data;;
+	newnode->next = NULL;
+	return (newnode);
+}
+
+/**
+ * add_nodeint- function add node at beginning
+ * @head: parameter
+ * @n: parameter
+ * Return: return null or newnode
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode = creatnode(n);
+
+	if (!head || !newnode)
+		return (NULL);
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
+
