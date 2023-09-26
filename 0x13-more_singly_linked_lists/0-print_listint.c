9bd3ed0ec2cfe4b2fffaab00e0a2c58502f32f84
root <root@YARA>
2023-09-27 01:38:17 +0300

    this is the  0 task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint-function to print integer
+ * @h: parameter
+ * Return:return no.of element
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("%d", h->n)
+			i++;
+		h->next;
+	}
+	return (i);
+}
+
