78ae0950e4eb6eb7821e8ab32f1ef7b5cc9732cd
root <root@YARA>
2023-09-27 05:22:32 +0300

    this is the  9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode = creatnode(n), *node;
 	unsigned int i = 0;
 
-	if (!head || !newnode || !*head)
+	if (!head || !newnode)
 		return (NULL);
 	if (!idx)
 	{
