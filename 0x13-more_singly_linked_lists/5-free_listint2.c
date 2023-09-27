6b80b3460a3272df7ee74e32be767ed0ed4ac4e0
root <root@YARA>
2023-09-27 03:27:31 +0300

    this is the  5 task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node = *head;
 
-	if (!head)
+	if (!head || !*head))
 		return;
 	while (*head)
-	{
+	z{
 		node = (*head);
 		(*head) = (*head)->next;
 		free(node);
