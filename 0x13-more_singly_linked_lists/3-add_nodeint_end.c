44ef22a60fc3987cabbffeb8bea76857773ce0af
root <root@YARA>
2023-09-27 03:53:23 +0300

    this is the  5 task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,26 +1,10 @@
 #include "lists.h"
 
 /**
- * creatnode- function create node
- * @data: parameter
- * Return: return newnode
- */
-listint_t *creatnode(const int data)
-{
-	listint_t *newnode = malloc(sizeof(listint_t));
-
-	if (!newnode)
-		return (NULL);
-	newnode->n = data;
-	newnode->next = NULL;
-	return (newnode);
-}
-
-/**
  * add_nodeint_end- function to add node in the end
  * @head: parameter
  * @n:parameter
- * Return: return new node
+ * Return: return
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
