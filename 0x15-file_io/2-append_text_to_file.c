c64442ee793a6a7c9bee1098d162bd2f1fc34021
root <yara200359@gmail.com>
2023-10-03 05:57:56 +0300

    task 0

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,8 +13,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename || !text_content)
 		return (-1);
-	while (text_content[len] != '\0')
+	if (text_content != NULL)
 	{
+		for (len = 0; text_content[len];)
 		++len;
 	}
 
