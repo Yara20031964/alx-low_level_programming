bb4e72d494c3fe4d2cb34f82d0044795e5ad009f
root <yara200359@gmail.com>
2023-10-03 05:45:41 +0300

    task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	while (*text_content != '\0')
+	while (text_content[len] != '\0')
 	{
 		++len;
-		++text_content;
 	}
 
 	fb = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
