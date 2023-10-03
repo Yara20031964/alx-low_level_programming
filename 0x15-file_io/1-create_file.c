d5577d67e37220ab5395d30181789b401f9eb980
root <yara200359@gmail.com>
2023-10-03 05:47:53 +0300

    task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fb, x, len = 0;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 	while (text_content[len] != '\0')
 	{
