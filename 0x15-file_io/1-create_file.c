72920e1b23087c605772f4157fc6c92f0e1d8af1
root <yara200359@gmail.com>
2023-10-03 05:42:38 +0300

    task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,9 @@ int create_file(const char *filename, char *text_content)
 
 	fb = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	x = write(fb, text_content, len);
+
+	if (fb == -1 || x == -1)
+		return (-1);
 	close(fb);
 	return (1);
 }
